578b4034a31e3bb05449a69bd87877f27306c6ff
shabrul.islam <shabrul2451@gmail.com>
2022-04-13 11:18:49 +0600

    minor change

--- core/v1/process_event.go
+++ core/v1/process_event.go
@@ -2,6 +2,6 @@ package v1
 
 // PipelineProcessEvent Pipeline ProcessEvent struct
 type PipelineProcessEvent struct {
-	ProcessId string                 `bson:"process_id"`
-	Data      map[string]interface{} `bson:"data"`
+	ProcessId string                 `bson:"process_id" json:"process_id"`
+	Data      map[string]interface{} `bson:"data" json:"data"`
 }

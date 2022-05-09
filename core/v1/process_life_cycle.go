63501eb163af6eccf5b9b0461f14d17c1aee5e45
shabrul.islam <shabrul2451@gmail.com>
2022-05-09 12:07:47 +0600

    resolve json bson issue

--- core/v1/process_life_cycle.go
+++ core/v1/process_life_cycle.go
@@ -7,14 +7,14 @@ import (
 
 // ProcessLifeCycleEvent Pipeline ProcessLifeCycleEvent struct
 type ProcessLifeCycleEvent struct {
-	ProcessId string               `bson:"process_id" json :"process_id"`
-	Step      string               `bson:"step" json :"step"`
-	StepType  enums.STEP_TYPE      `bson:"step_type" json :"step_type"`
-	Status    enums.PROCESS_STATUS `bson:"status" json :"status"`
-	Next      []string             `bson:"next" json :"next"`
-	Agent     string               `bson:"agent" json :"agent"`
-	Pipeline  *Pipeline            `bson:"pipeline" json :"pipeline"`
-	CreatedAt time.Time            `bson:"created_at" json :"created_at"`
-	Trigger   enums.TRIGGER        `bson:"trigger" json :"trigger"`
-	Claim int  `bson:"claim" json:"claim"`
+	ProcessId string               `bson:"process_id" json:"process_id"`
+	Step      string               `bson:"step" json:"step"`
+	StepType  enums.STEP_TYPE      `bson:"step_type" json:"step_type"`
+	Status    enums.PROCESS_STATUS `bson:"status" json:"status"`
+	Next      []string             `bson:"next" json:"next"`
+	Agent     string               `bson:"agent" json:"agent"`
+	Pipeline  *Pipeline            `bson:"pipeline" json:"pipeline"`
+	CreatedAt time.Time            `bson:"created_at" json:"created_at"`
+	Trigger   enums.TRIGGER        `bson:"trigger" json:"trigger"`
+	Claim     int                  `bson:"claim" json:"claim"`
 }

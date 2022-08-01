bb3b84e078bfc9e05c88a82dda43c93c6817f7d8
niloydeb1 <niloymishu@gmail.com>
2022-08-01 12:34:56 +0600

    Fix #15 added bson in pipeline type

--- core/v1/pipeline.go
+++ core/v1/pipeline.go
@@ -4,13 +4,13 @@ import "errors"
 
 // Pipeline Pipeline struct
 type Pipeline struct {
-	MetaData   PipelineMetadata    `json:"_metadata" yaml:"_metadata"`
-	Option     PipelineApplyOption `json:"option" yaml:"option"`
-	ApiVersion string              `json:"api_version" yaml:"api_version"`
-	Name       string              `json:"name"  yaml:"name"`
-	ProcessId  string              `json:"process_id" yaml:"process_id"`
-	Label      map[string]string   `json:"label" yaml:"label"`
-	Steps      []Step              `json:"steps" yaml:"steps"`
+	MetaData   PipelineMetadata    `json:"_metadata" yaml:"_metadata" bson:"_metadata"`
+	Option     PipelineApplyOption `json:"option" yaml:"option" bson:"option"`
+	ApiVersion string              `json:"api_version" yaml:"api_version" bson:"api_version"`
+	Name       string              `json:"name"  yaml:"name" bson:"name"`
+	ProcessId  string              `json:"process_id" yaml:"process_id" bson:"process_id"`
+	Label      map[string]string   `json:"label" yaml:"label" bson:"label"`
+	Steps      []Step              `json:"steps" yaml:"steps" bson:"steps"`
 	Claim      int                 `json:"claim" yaml:"claim"`
 }
 

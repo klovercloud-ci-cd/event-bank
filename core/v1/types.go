856332637ca726c77a1480a68041ed4650b68a05
shabrul.islam <shabrul2451@gmail.com>
2022-08-24 13:10:18 +0600

    fixed #32 Add allowed branches in pipeline metadata

--- core/v1/types.go
+++ core/v1/types.go
@@ -66,6 +66,7 @@ type CompanyMetadata struct {
 type PipelineMetadata struct {
 	CompanyId       string          `json:"company_id" yaml:"company_id" bson:"company_id"`
 	CompanyMetadata CompanyMetadata `json:"company_metadata" yaml:"company_metadata" bson:"company_metadata"`
+	AllowedBranches string          `json:"allowed_branches" bson:"allowed_branches"`
 }
 
 // PipelineStatusCount pipeline status count info
